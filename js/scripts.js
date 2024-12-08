1eef01b14137d54d077bad329ccdf1a3c0beb15c
adinaweiss <168139846+adinaweiss@users.noreply.github.com>
2024-12-07 23:45:27 -0500

    who knows

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
+    const lightboxCaption = document.getElementById('lightbox-caption');
     const closeBtn = document.querySelector('.lightbox-content .close');
     const prevArrow = document.querySelector('.arrow.prev');
     const nextArrow = document.querySelector('.arrow.next');
-    const images = Array.from(document.querySelectorAll('#gallery img'));
+    const images = Array.from(document.querySelectorAll('#gallery .gallery-item img'));
     let currentIndex = -1;
 
     function showLightbox(index) {
         currentIndex = index;
         lightboxImg.src = images[currentIndex].src;
+        lightboxCaption.textContent = images[currentIndex].parentNode.querySelector('.gallery-caption').textContent;
         lightbox.classList.add('visible');
     }
 
@@ -19,12 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function prevImage() {
         currentIndex = (currentIndex - 1 + images.length) % images.length;
-        lightboxImg.src = images[currentIndex].src;
+        showLightbox(currentIndex);
     }
 
     function nextImage() {
         currentIndex = (currentIndex + 1) % images.length;
-        lightboxImg.src = images[currentIndex].src;
+        showLightbox(currentIndex);
     }
 
     images.forEach((img, index) => {
